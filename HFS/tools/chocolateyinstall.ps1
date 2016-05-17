1d70cfd6aa073f76d5916b5a2672b972c0f328b9
z00nx 0 <z00nx0@gmail.com>
2016-05-17 23:58:37 +1000

    Fixed review comments

--- HFS/tools/chocolateyinstall.ps1
+++ HFS/tools/chocolateyinstall.ps1
@@ -1,5 +1,5 @@
-$packageName= 'hfs' # arbitrary name for the package, used in messages
+$packageName= 'hfs'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'http://www.melauto.it/hfs/hfs.exe' # download url
+$url        = 'http://www.melauto.it/hfs/hfs.exe'
 $hfsPath = Join-Path $toolsDir 'hfs.exe'
 Get-ChocolateyWebFile $packageName $hfsPath $url
